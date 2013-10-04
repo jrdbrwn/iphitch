c316bb592ecc109ad393205404523e981d0bf987
jrdbrwn <b.jared@gmail.com>
2013-10-04 19:19:34 -0400

    Fixed where an echo was getting in the way of returning error from curl.

--- iphitch.sh
+++ iphitch.sh
@@ -36,7 +36,8 @@ while [  $SITE_COUNT -lt ${#GET_IP_SITES[@]} ]; do
 # -f fail silently
 # -w write out display on success
 echo -e "\nConnecting to:${GET_IP_SITES[$SITE_POSITION]} [$SITE_POSITION]..."
-OUTPUT=$( curl -qSfsw '\n%{http_code}' ${GET_IP_SITES[$SITE_POSITION]} ) 2>/dev/null 
+OUTPUT=$( curl -qSfsw '\n%{http_code}' ${GET_IP_SITES[$SITE_POSITION]} ) 2>/dev/null
+#echo $OUTPUT
 #Gets curl's exit code
 ERROR=$?
 
@@ -44,7 +45,7 @@ ERROR=$?
 CURLEDIP=$( echo "$OUTPUT" | head -n-1 ) 
 STOREDIP=$( cat $IP_FILE )
 echo -e "$( date +%Y-%m-%d\(%H:%M:%S\) ) Curled IP: $CURLEDIP Curl Error:$ERROR - HTTP Error: $HTTPSTATUS" >> $LOG_FILE
-#echo "$ERROR"
+echo "$ERROR"
 
 	#Here we do some error checking on the exit code from curl
 	if [ $ERROR -ne 0 ] ; then
@@ -72,6 +73,8 @@ echo -e "$( date +%Y-%m-%d\(%H:%M:%S\) ) Curled IP: $CURLEDIP Curl Error:$ERROR 
 	elif [[ $ERROR -eq 7 ]]; then
 
 		echo -e "$( date +%Y-%m-%d\(%H:%M:%S\) ) Couldn't resolve host. The given remote host was not resolved." >> $LOG_FILE
+		let SITE_COUNT=SITE_COUNT+1
+		SITE_POSITION=$SITE_COUNT
 	else
 		#If the curled IP is different
 		if [[ "$CURLEDIP" != "$STOREDIP" ]]; then
@@ -86,7 +89,7 @@ echo -e "$( date +%Y-%m-%d\(%H:%M:%S\) ) Curled IP: $CURLEDIP Curl Error:$ERROR 
 
 			while [  $EMAIL_COUNT -lt ${#EMAILS[@]} ]; do
 				echo "${GET_IP_SITES[$SITE_COUNT]} returns $CURLEDIP" | mail -s "The IP for $HOSTNAME has changed" ${EMAILS[$EMAIL_COUNT]}
-
+				echo -e "$( date +%Y-%m-%d\(%H:%M:%S\) ) $CURLEDIP has been emailed to ${#EMAILS[@]}" >> $LOG_FILE
 				let EMAIL_COUNT=EMAIL_COUNT+1
 			done	
 		fi
